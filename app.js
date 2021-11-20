ea23662e312842c1f08ceb9d76574c7fcdd58e95
dadagofafa <fu6fu62022@gmail.com>
2021-11-20 09:47:40 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
+require('./config/mongoose')
+
 
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost/short-url-generator')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs(({ defaultLayout: 'main', extname: '.hbs' })))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
   res.render('index')
