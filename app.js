0ee1a75fcd9a9574c0e470eff3ab76826a6373bd
dadagofafa <fu6fu62022@gmail.com>
2021-11-20 10:32:07 +0800

    add the get('/:id'), post('/')

--- app.js
+++ app.js
@@ -17,8 +17,19 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/result', (req, res) => {
-  res.render('result')
+app.get('/:id', (req, res) => {
+  const id = req.params.id
+  url.find({ shortUrl: id })
+    .then(data => res.redirect(data[0].originalUrl))
+    .catch(error => console.log(error))
+})
+
+app.post('/', (req, res) => {
+  const shortUrl = random5digits()
+  url.findOne({ originalUrl: req.body.originalUrl })
+    .then(data => data ? data : url.create({ originalUrl: req.body.originalUrl, shortUrl }))
+    .then(data => res.render('result', { shortUrl: data.shortUrl, originalUrl: req.body.originalUrl }))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
