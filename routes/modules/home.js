33bef7b402b3dca137cb0402f2996a1f319911ea
dadagofafa <fu6fu62022@gmail.com>
2021-11-20 16:50:21 +0800

    檔名及命名調整

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const url = require('../../models/url')
+const URL = require('../../models/URL')
 
 router.get('/', (req, res) => {
   res.render('index')
