2476f5a4b51f798a2586b2a9e40131830d00b468
dadagofafa <fu6fu62022@gmail.com>
2021-11-20 10:54:06 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
-require('./config/mongoose')
 
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
 const url = require('./models/url')
 const random5digits = require('./random5digits')
+
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -11,24 +13,12 @@ const port = 3000
 app.engine('hbs', exphbs(({ defaultLayout: 'main', extname: '.hbs' })))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
-
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.get('/:id', (req, res) => {
   const id = req.params.id
   url.find({ shortUrl: id })
     .then(data => res.redirect(data[0].originalUrl))
-    .catch(error => console.log(error))
-})
-
-app.post('/', (req, res) => {
-  const shortUrl = random5digits()
-  url.findOne({ originalUrl: req.body.originalUrl })
-    .then(data => data ? data : url.create({ originalUrl: req.body.originalUrl, shortUrl }))
-    .then(data => res.render('result', { shortUrl: data.shortUrl, originalUrl: req.body.originalUrl }))
     .catch(error => console.log(error))
 })
 
