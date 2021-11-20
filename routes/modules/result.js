9d7dc5a27a2dbe6278b4afe47566120bbc3cb57d
dadagofafa <fu6fu62022@gmail.com>
2021-11-20 17:10:17 +0800

    修改檔名跟命名

--- routes/modules/result.js
+++ routes/modules/result.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const url = require('../../models/url')
+const URL = require('../../models/URL')
 const random5digits = require('../../random5digits')
 
 router.post('/', (req, res) => {
     const shortUrl = random5digits()
-    url.findOne({ originalUrl: req.body.originalUrl })
-      .then(data => data ? data : url.create({ originalUrl: req.body.originalUrl, shortUrl }))
+    URL.findOne({ originalUrl: req.body.originalUrl })
+      .then(data => data ? data : URL.create({ originalUrl: req.body.originalUrl, shortUrl }))
       .then(data => res.render('result', { shortUrl: data.shortUrl, originalUrl: req.body.originalUrl }))
       .catch(error => console.log(error))
   })
