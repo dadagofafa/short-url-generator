ed28f5f6ff97c555df2afcfeb1320bee31680717
dadagofafa <fu6fu62022@gmail.com>
2021-11-27 10:54:27 +0800

    新增驗證輸入值不能為空白或非網址資料

--- routes/modules/result.js
+++ routes/modules/result.js
@@ -4,11 +4,12 @@ const URL = require('../../models/URL')
 const random5digits = require('../../random5digits')
 
 router.post('/', (req, res) => {
-    const shortUrl = random5digits()
-    URL.findOne({ originalUrl: req.body.originalUrl })
-      .then(data => data ? data : URL.create({ originalUrl: req.body.originalUrl, shortUrl }))
-      .then(data => res.render('result', { shortUrl: data.shortUrl, originalUrl: req.body.originalUrl }))
-      .catch(error => console.log(error))
+  if (!req.body.url) return res.redirect("/")
+  const shortUrl = random5digits()
+  URL.findOne({ originalUrl: req.body.originalUrl })
+    .then(data => data ? data : URL.create({ originalUrl: req.body.originalUrl, shortUrl }))
+    .then(data => res.render('result', { shortUrl: data.shortUrl, originalUrl: req.body.originalUrl }))
+    .catch(error => console.log(error))
   })
 
 module.exports = router
